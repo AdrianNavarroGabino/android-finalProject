ad9835f6acb7cdf4b01312fe71b03919f21d707f
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-02-10 11:48:53 +0100

    Floating button

---
+++ app/src/main/java/com/example/gastos/AnyadirGasto.kt
@@ -0,0 +1,12 @@
+package com.example.gastos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnyadirGasto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anyadir_gasto)
+    }
+}
--- app/src/main/java/com/example/gastos/GastosActivity.kt
+++ app/src/main/java/com/example/gastos/GastosActivity.kt
@@ -1,8 +1,13 @@
 package com.example.gastos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_gastos.*
+
 
 class GastosActivity : AppCompatActivity() {
 
@@ -16,5 +21,52 @@ class GastosActivity : AppCompatActivity() {
 
         usuarioLbl.text = "¡Hola ".plus(nombre.capitalize()).plus("!")
         ultimoAccesoLbl.text = "Último acceso: ".plus(ultimoAcceso)
+
+        anyadir.setOnClickListener{
+            val builder =
+                AlertDialog.Builder(this)
+            builder.setTitle("Elige una opción")
+
+            val options: MutableList<String> = ArrayList()
+            options.add("Añadir gasto")
+            options.add("Añadir ingreso")
+            options.add("Añadir categoría")
+            options.add("Añadir cuenta")
+
+            val dataAdapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_dropdown_item_1line, options
+            )
+            builder.setAdapter(
+                dataAdapter
+            ) { dialog, which ->
+                when(which)
+                {
+                    0 -> {
+                        val myIntent = Intent(this, AnyadirGasto::class.java)
+                        startActivityForResult(myIntent, 1110)
+                    }
+                    1 -> {
+                        val myIntent = Intent(this, AnyadirGasto::class.java)
+                        startActivityForResult(myIntent, 1111)
+                    }
+                    2 -> {
+                        val myIntent = Intent(this, AnyadirGasto::class.java)
+                        startActivityForResult(myIntent, 1112)
+                    }
+                    3 -> {
+                        val myIntent = Intent(this, AnyadirGasto::class.java)
+                        startActivityForResult(myIntent, 1113)
+                    }
+                }
+                Toast.makeText(
+                    applicationContext,
+                    "Has elegido " + options[which],
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            val dialog = builder.create()
+            dialog.show()
+        }
     }
 }
